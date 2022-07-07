1e5a283184819bcd48aef0c6bf3bdaea8bf8792e
kartik mathur <kartik.mathur10@gmail.com>
2022-07-07 21:10:24 +0530

    Lecture-31 DMA

--- Lecture-31/2dArray.cpp
+++ Lecture-31/2dArray.cpp
@@ -9,6 +9,9 @@ int main() {
 	int ** a;
 	DMA2D(a, n, m);
 
+	int **b;
+	DMA2D(b, 10, 5);
+
 	// int **a = new int*[n];
 
 	// for (int i = 0; i < n; ++i)
@@ -30,6 +33,24 @@ int main() {
 		for (int j = 0; j < m; ++j)
 		{
 			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	number = 1;
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			b[i][j] = number++;
+		}
+	}
+
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			cout << b[i][j] << " ";
 		}
 		cout << endl;
 	}
